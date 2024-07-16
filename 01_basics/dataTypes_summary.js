88841306a13634a6267295db347f6aa392989a61
mtalal0786 <talalshahbaz000786@gmail.com>
2024-07-16 14:14:18 +0000

    Memory Types Used

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -1,7 +1,9 @@
 //Basically there are Only 2 Types of Data.They are Further divided into many other Types.
 
-//Premetive / Call By Value  
+//Primitive / Call By Value  
+
 //7 Types. 1) String 2) Number 3) Boolean 4) null 5) undefined 6) Symbol 7) BigInt
+
 const marks = 100 //Number
 const name = "Talal" //String
 const isLoggedIn = true //Boolean
@@ -14,7 +16,10 @@ const anotherId = Symbol('123')
 const bigNumber = 4554553454453444345n // BigInt
 //use n at end of number to spacify its type as BigInt
 // console.log(typeof bigNumber);
-//Non-Premetive / Call By Reference
+
+
+//Non-Primitive / Call By Reference
+
 // 1) Arrays 2) Objects 3) Functions
 
 fruits = ["Banana","Mango","Apple","Orange"] //Array
@@ -35,3 +40,30 @@ const myFunction = function(){
 
 //JavaScript is the Dynamically Typed Language as 
 //It Does not Required to describe the type of values when Intialized. 
+
+//*********************************************************************************
+
+        //Memory Alocation and Types
+
+// Stack => Primitive (copy of data)
+
+let myName = "Talal"
+let anotherName = myName //give copy of data stored in variable.
+anotherName = "M Talal" //changing in this variable value does not effect myName value.
+
+console.log(myName) 
+console.log(anotherName) 
+
+
+// Heap => Non-Primitive (actual data)
+
+let userOne = {
+    name: "Talal",
+    email: "talal@gmail.com"
+}
+let userTwo = userOne // gives the same object to the second user
+userTwo.name = "M Talal" // changing in the value of userTwo also change value of userOne
+//Because they both refer to same object not two seperate objects as done in primitive DT.
+
+console.log(userOne); 
+console.log(userTwo);  
