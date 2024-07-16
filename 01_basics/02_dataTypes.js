1d2f4444bf9ced9d278c1d888abd3e234f136699
mtalal0786 <talalshahbaz000786@gmail.com>
2024-07-16 07:41:47 +0000

    davcontainer

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";
+// alert(0) // not allowed in node js
+let name = "Talal"
+let age = 22
+let isLoggedIn = true
+// number = 4
+// string = ""
+// boolean = false
+// null
+// undefined
+// Symbol unique
+// object
+// typeof used to find the type of any value
+// Type of null is object 
+console.log(typeof(null));
